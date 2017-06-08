2f5108ee8363f62fc9711dd9c3c0d4fbe9591c51
mattiuw <mattiuw@users.noreply.github.com>
2017-06-08 15:01:54 +0200

    Add files via upload

---
+++ fisica.py
@@ -0,0 +1,11 @@
+'''Appunti e formule di fisica (U.Amaldi, Le traiettorie della fisica volume 1'''
+
+#   FORMULE
+#Velocità
+def vel(distanza_percorsa,tempo_impiegato):
+    s = float(distanza_percorsa)
+    t = float(tempo_impiegato)
+
+    v = s/t
+
+    print(v, r'm/s')
